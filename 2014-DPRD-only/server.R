e13500c441279e1a3f3c214090137cd78d0bfa0c
Ainun Najib <ainunnajib@gmail.com>
2015-03-12 08:19:12 +0800

    filter and sum

--- 2014-DPRD-only/server.R
+++ 2014-DPRD-only/server.R
@@ -3,20 +3,31 @@ library(data.table)
 load("DPRDonly.RData")
 
 shinyServer(function(input, output) {
-  output$table <- renderDataTable({
-    DPRDonly
-  }
-  #, options = list(
-  #  columnDefs = list(list(targets = c(4, 5, 6, 7), type = "num-fmt"))
-  #)
-  )
-    
-  output$downloadData <- downloadHandler(
-    filename = 'Delta-2014-P-vs-2014.csv',
-    content = function(file) {
-      write.table(format(DPRDonly, scientific = FALSE, trim = TRUE, big.mark = "."),
-                  file = file, sep = ",", row.names = FALSE, quote = TRUE)
-    }
-  )
+  filter <- reactive({ input$filter })
   
+  output$table <- renderDataTable({
+    DPRDonly[grep(filter(), Kegiatan, ignore.case = TRUE)]
+  })
+  
+  output$total <- renderText({ 
+    format(
+      sum(as.numeric(
+        gsub('\\.', '',
+             DPRDonly[grep(filter(), Kegiatan, ignore.case = TRUE)]$PembahasanDPRD.Baru.P)),
+        na.rm = TRUE),
+      scientific = FALSE, trim = TRUE, big.mark = "."
+    )
+  })
+    
+  datasetInput <- reactive({
+    DPRDonly[grep(filter(), Kegiatan, ignore.case = TRUE)]
+  })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() { paste0('Delta-2014-P-vs-2014-', filter(), '.csv') },
+    content = function(file) {
+      write.table(format(datasetInput(), 
+                         scientific = FALSE, trim = TRUE, big.mark = "."),
+                  file = file, sep = ",", row.names = FALSE, quote = TRUE)
+    })
 })
--- 2014-DPRD-only/ui.R
+++ 2014-DPRD-only/ui.R
@@ -2,8 +2,13 @@ library(shiny)
 load("DPRDonly.RData")
 
 shinyUI(
-  fluidPage(    
-    fluidRow( downloadButton('downloadData', 'Download') ),
+  fluidPage(
+    h1("Data tambahan DPRD --- Delta 2014-P vs 2014 (ada di 2014-P namun tidak di 2014) yang kolom Anggaran di 2014-P kosong."),
+    fluidRow( textInput("filter", "Filter Kegiatan", value = ""), 
+              h4(paste("Total PembahasanDPRD.Baru.P = Rp. "),
+                 textOutput("total", inline = TRUE)),
+              downloadButton('downloadData', 'Download') ),
+    fluidRow(  ),
     fluidRow(
       dataTableOutput(outputId="table")
     )    
